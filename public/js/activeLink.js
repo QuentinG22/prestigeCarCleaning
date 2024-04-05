a5cb37a502428c089624cf92051da29384f0be14
QuentinG22 <quentin-guillemin@orange.fr>
2024-04-05 20:13:10 +0200

    Modification du script pour ajouter la class active sur les liens du menu par rapport à leurs pages.

--- public/js/activeLink.js
+++ public/js/activeLink.js
@@ -1,16 +1,26 @@
-// let baseUrl = '/stagiaires-kercode/quentin-guillemin/prestigeCarCleaning/';
-let baseUrl = '/prestigeCarCleaning/';
-let url = window.location.pathname;
+// Récupérer l'URL complète
+let fullUrl = window.location.href;
+
+// Sélectionner tous les liens dans le menu
 let links = document.querySelectorAll('.mainMenu ul li a');
 
-if (url === baseUrl) {
-    let home = links[0];
-    home.classList.add('active');
- } else {
-    links.forEach(link => {
-        let linkUrl = link.href;
-        if (url === linkUrl) {
-            link.classList.add('active');
-        }
-    })
+// Parcourir tous les liens
+let matchFound = false;
+links.forEach(link => {
+    let linkUrl = link.href;
+
+    // Comparer l'URL actuelle avec l'URL du lien
+    if (fullUrl === linkUrl) {
+        // Ajouter la classe active si les URL correspondent
+        link.classList.add('active');
+        matchFound = true;
+    } else {
+        // Retirer la classe active si les URL ne correspondent pas
+        link.classList.remove('active');
+    }
+});
+
+// Si aucun lien ne correspond à l'URL actuelle, activer le lien "Accueil"
+if (!matchFound) {
+    links[0].classList.add('active');
 }
